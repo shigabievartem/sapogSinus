66495f1705d1111a6d52f4c29e001d7022c8569b
nrodichenko <bornikita@gmail.com>
2019-03-20 19:23:29 +0300

    Implement basic serial communication

--- src/main/java/sample/utils/SerialDevice.java
+++ src/main/java/sample/utils/SerialDevice.java
@@ -7,10 +7,12 @@ import jssc.SerialPortException;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import sample.utils.callbacks.OnParamLoadListener;
-import sample.utils.callbacks.OnParamSaveListener;
+import javafx.scene.control.TextArea;
 
+import java.awt.*;
 import java.io.IOException;
+
+import static java.lang.String.format;
 
 public class SerialDevice {
     private static final Logger LOG = LoggerFactory.getLogger(SerialDevice.class);
@@ -28,8 +30,28 @@ public class SerialDevice {
     private boolean traceWriteFailures = true;
 
     private SerialState portState = SerialState.NOT_OPEN;
-    private OnParamSaveListener psaveListener = null;
-    private OnParamLoadListener ploadListener = null;
+
+    private TextArea mainConsole = null;
+
+    Runnable serialReader = new Runnable() {
+        public void run() {
+            StringBuilder message = new StringBuilder();
+            System.out.println("Starting serialReader thread");
+            while (true) {
+                byte buffer[];
+                try {
+                    if ((buffer = port.readBytes()) != null) {
+                        for (byte b : buffer) message.append((char) b);
+                        logToConsole(message.toString().getBytes());
+                        message.setLength(0);
+                    }
+                } catch (SerialPortException e) {
+                    //logToConsole(e.toString().getBytes());
+                    System.out.println(format("Exception in serialReader thread: %s", e));
+                }
+            }
+        }
+    };
 
     public SerialDevice(@NotNull String name, @NotNull String newDevice, @NotNull int newBaud) throws IOException, SerialPortException {
         // TODO Refactor
@@ -62,6 +84,10 @@ public class SerialDevice {
         tryReopen();
     }
 
+    public void setConsole(@NotNull TextArea console) {
+        mainConsole = console;
+    }
+
     public void tryReopen() throws SerialPortException {
         port = new SerialPort(device);
         int jsscParity = 0;
@@ -77,6 +103,7 @@ public class SerialDevice {
             port.openPort();
             port.setParams(baudRate, dataBits, stopBits, jsscParity);
             portState = SerialState.IDLE;
+            new Thread(serialReader).start();
         } catch (SerialPortException e) {
             LOG.error("Cannot open port {}, retrying in {}ms", getPortSpec(), reopenTimeoutMS, e);
             //reopenAt = System.currentTimeMillis() + reopenTimeoutMS;
@@ -162,34 +189,89 @@ public class SerialDevice {
     }
 
     private boolean isPortBusy() {
-        return (portState == SerialState.PARAM_SAVING) || (portState == SerialState.PARAMS_LOADING);
+        return (portState == SerialState.PARAM_SAVING) || (portState == SerialState.PARAM_LOADING);
     }
 
-    public void saveParam(String fieldName, Object value) throws SerialPortException {
-        if (!isPortBusy()) {
-            //TODO send param save string
-            portState = SerialState.PARAM_SAVING;
-            byte[] buffer;
-            if (value instanceof Float) {
-                buffer = String.format("cfg set %s %.2f\n", fieldName, (Float)value).getBytes();
-            } else if (value instanceof Boolean) {
-                buffer = String.format("cfg set %s %s\n", fieldName, (Boolean)value?"true":"false").getBytes();
-            } else if (value instanceof Integer) {
-                buffer = String.format("cfg set %s %df\n", fieldName, (Integer)value).getBytes();
-            } else return; //TODO throw something
-            port.writeBytes(buffer);
-        } else {
-            //TODO throw something
+    private void logToConsole(byte[] buffer) {
+        if (mainConsole != null) SapogUtils.printDirect(mainConsole, new String(buffer));
+    }
+
+    private void tryWriteBytes(byte[] buffer) throws SerialPortException, IOException {
+        try {
+            boolean success = port.writeBytes(buffer);
+            if (success) {
+                //logToConsole(buffer);
+                portState = SerialState.IDLE;
+            } else {
+                portState = SerialState.NOT_OPEN;
+                port.closePort();
+                throw new IOException(String.format("Failed to write to %s, closing", getPortSpec()));
+            }
+        }
+        catch (SerialPortException e) {
+            //TODO do better?
+            throw e;
         }
     }
 
-    public Object loadParams(String fieldName) throws IOException {
-        if (!isPortBusy()) {
-            //TODO send param load string
-            portState = SerialState.PARAMS_LOADING;
+    public void sendString(String str) throws SerialPortException, IOException {
+        if (isPortBusy()) throw new IOException("Port busy with another operation");
+        tryWriteBytes(str.concat("\r\n").getBytes());
+    }
+
+    public void saveParam(String fieldName, Object value) throws SerialPortException, IOException {
+        if (isPortBusy()) throw new IOException("Port busy with another operation");
+        portState = SerialState.PARAM_SAVING;
+        byte[] buffer;
+        if (value instanceof Float) {
+            buffer = String.format("cfg set %s %.2f\r\n", fieldName, value).getBytes();
+        } else if (value instanceof Boolean) {
+            buffer = String.format("cfg set %s %s\r\n", fieldName, (Boolean)value?"true":"false").getBytes();
+        } else if (value instanceof Integer) {
+            buffer = String.format("cfg set %s %d\r\n", fieldName, value).getBytes();
         } else {
-            //TODO throw IOException
+            portState = SerialState.IDLE;
+            return; //TODO throw something
         }
+
+        tryWriteBytes(buffer);
+
+        portState = SerialState.PARAM_SAVED;
+    }
+
+    private String readLineBlocking() throws SerialPortException {
+        StringBuilder message = new StringBuilder();
+        try {
+            while (true) {
+                byte buffer[];
+                if ((buffer = port.readBytes()) != null) {
+                    for (byte b : buffer) {
+                        if (b == '>') {
+                            message.setLength(0);
+                        } else if ((b == '\r') || (b == '\n')) {
+                            return message.toString();
+                        } else {
+                            message.append((char) b);
+                        }
+                    }
+                }
+            }
+        }
+        catch (SerialPortException ex) {
+            System.out.println(ex);
+            System.out.println("serialEvent");
+        }
+        return "";
+    }
+
+    public Object loadParam(String fieldName) throws SerialPortException, IOException {
+        if (isPortBusy()) throw new IOException("Port busy with another operation");
+        //TODO send param load string
+        portState = SerialState.PARAM_LOADING;
+        byte[] buffer = String.format("cfg get %s\r\n", fieldName).getBytes();
+        tryWriteBytes(buffer);
+        portState = SerialState.PARAM_LOADED;
+
         return null;
     }
 
