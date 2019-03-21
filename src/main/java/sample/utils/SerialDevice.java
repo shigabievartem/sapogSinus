548fdc7811df3fd78f4795a5ee2ed3b28210ed55
nrodichenko <bornikita@gmail.com>
2019-03-21 10:54:06 +0300

    Разные добавления
    Добавлены парсеры для разных типов сообщений (параметры, stat2)
    Изменен вызов ConnectionInfo
    Разные мелочи

--- src/main/java/sample/utils/SerialDevice.java
+++ src/main/java/sample/utils/SerialDevice.java
@@ -1,22 +1,23 @@
 package sample.utils;
 
-import com.typesafe.config.Config;
-import com.typesafe.config.ConfigFactory;
+import com.fasterxml.jackson.databind.node.NullNode;
 import jssc.SerialPort;
 import jssc.SerialPortException;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import javafx.scene.control.TextArea;
+import sample.objects.ConnectionInfo;
 
-import java.awt.*;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 import static java.lang.String.format;
 
 public class SerialDevice {
     private static final Logger LOG = LoggerFactory.getLogger(SerialDevice.class);
-    public static Config CONFIG_DEFAULTS = ConfigFactory.parseResources(SerialDevice.class, "/serial-defaults.conf");
 
     private String name;
     private String device;
@@ -33,25 +34,25 @@ public class SerialDevice {
 
     private TextArea mainConsole = null;
 
-    Runnable serialReader = new Runnable() {
-        public void run() {
-            StringBuilder message = new StringBuilder();
-            System.out.println("Starting serialReader thread");
-            while (true) {
-                byte buffer[];
-                try {
-                    if ((buffer = port.readBytes()) != null) {
-                        for (byte b : buffer) message.append((char) b);
-                        logToConsole(message.toString().getBytes());
-                        message.setLength(0);
-                    }
-                } catch (SerialPortException e) {
-                    //logToConsole(e.toString().getBytes());
-                    System.out.println(format("Exception in serialReader thread: %s", e));
-                }
-            }
-        }
-    };
+    private boolean readThreadShouldExit = false;
+
+    public Map<String, Object> getCurrentParamMap() {
+        final HashMap<String, Object> integerObjectHashMap = new HashMap<>(currentParamMap);
+        return integerObjectHashMap;
+    }
+
+    /* Internal values */
+    private double voltage = 0.0f;
+    private double current = 0.0f;
+    private int RPM = 0;
+    private double DC = 0.0f;
+    private double temp = 0.0f;
+    private int limits = 0;
+    private int ZCF = 0;
+    private String version = "";
+
+
+    Map<String, Object> currentParamMap = new HashMap<>();
 
     public SerialDevice(@NotNull String name, @NotNull String newDevice, @NotNull int newBaud) throws IOException, SerialPortException {
         // TODO Refactor
@@ -81,7 +82,142 @@ public class SerialDevice {
         reopenTimeoutMS = 1000;
         traceWriteFailures = true;
 
+        currentParamMap.put("esc_base", 256);
+        currentParamMap.put("esc_index", 0);
+        currentParamMap.put("pwm_enable", false);
+        currentParamMap.put("mot_num_poles", 14);
+        currentParamMap.put("mot_dc_slope", 5f);
+        currentParamMap.put("mot_dc_accel", 0.09f);
+        currentParamMap.put("mot_pwm_hz", 20000);
+        currentParamMap.put("ctl_dir", false);
+        currentParamMap.put("temp_lim", 100);
+        currentParamMap.put("mot_i_max", 20);
+        currentParamMap.put("sens_i_scale", 1);
+        currentParamMap.put("dc_slider", 0.5f);
+        currentParamMap.put("set_rpm", 99);
+
         tryReopen();
+    }
+
+    public ConnectionInfo getConnectionInfo() {
+        return new ConnectionInfo(
+                getPortState() != SerialState.NOT_OPEN,
+                voltage,
+                current,
+                DC,
+                RPM);
+    }
+
+    Runnable serialReader = new Runnable() {
+        public void run() {
+            StringBuilder message = new StringBuilder();
+            System.out.println("Starting serialReader thread");
+            StringBuilder lastLine = new StringBuilder();
+            while (!readThreadShouldExit && (portState != SerialState.NOT_OPEN)) {
+                byte buffer[];
+                try {
+                    if ((buffer = port.readBytes()) != null) {
+                        for (byte b : buffer) {
+                            message.append((char) b);
+                            lastLine.append((char) b);
+                            if (b == '\n') {
+                                if (!tryExtractStat2(lastLine.toString())) {
+                                    if (!tryExtractParam(lastLine.toString()))
+                                    tryExtractVersion(lastLine.toString());
+                                }
+                                lastLine.setLength(0);
+                            }
+                        }
+                        logToConsole(message.toString().getBytes());
+                        message.setLength(0);
+                    }
+                } catch (SerialPortException e) {
+                    //logToConsole(e.toString().getBytes());
+                    System.out.println(format("Exception in serialReader thread: %s", e));
+                }
+            }
+            System.out.println("Closing serialReader thread");
+        }
+    };
+
+
+    private boolean tryExtractVersion(String s) {
+        int idxKey = s.indexOf("io.px4.sapog");
+        if ((idxKey == 0)) { //length of "stat2      \r\n"
+            String valStr = s.substring(0, s.length() - 2); //remove \r\n
+            version = valStr;
+        }
+        return false;
+    }
+
+    /*
+        STAT2<пробел>
+        напряжение XX.XX<пробел>
+        ток XXX.XX<пробел>
+        RPM XXXXX<пробел>
+        DC X.XX<пробел>
+        температура XXX.XX<пробел>
+        лимиты X<пробел>
+        ZCF XX<перевод строки>
+     */
+
+    private boolean tryExtractStat2(String s) {
+        int idxKey = s.indexOf("STAT2");
+        if ((idxKey == 0) && (s.length() > 15)) { //length of "stat2      \r\n"
+            String valStr = s.substring(idxKey + 6, s.length() - 2); //remove \r\n
+            String[] data = valStr.split(" ");
+            try {
+                voltage = Double.parseDouble(data[0]);
+                current = Double.parseDouble(data[1]);
+                RPM = Integer.parseInt(data[2]);
+                DC = Double.parseDouble(data[3]);
+                temp = Double.parseDouble(data[4]);
+                limits = Integer.parseInt(data[5]);
+                ZCF = Integer.parseInt(data[6]);
+                return true;
+            } catch (NumberFormatException e) {
+                System.out.println(format("Conversion error for stat2 string %s : %s", s, e.toString()));
+            }
+        }
+        return false;
+    }
+
+    private boolean tryExtractParam(String s) {
+        Iterator it = currentParamMap.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry pair = (Map.Entry)it.next();
+            String param = (String)pair.getKey();
+            Object oldVal = pair.getValue();
+            int idxKey = s.indexOf(param);
+            int idxBracket = s.indexOf("[");
+            int idxEq = s.indexOf("=");
+            if (
+                    (idxKey == 0) &&
+                    (idxEq > param.length()) &&
+                    (idxBracket > idxEq + 1)
+            ) {
+                String valStr = s.substring(idxEq + 1, idxBracket);
+                valStr = valStr.replaceAll("\\s","");
+                //valStr = valStr.replaceAll("\\.",",");
+                try {
+                    if (oldVal instanceof Float) {
+                        Float val = Float.parseFloat(valStr);
+                        currentParamMap.put(param, val);
+                    } else if (oldVal instanceof Boolean) {
+                        Boolean val = Boolean.parseBoolean(valStr);
+                        currentParamMap.put(param, val);
+                    } else if (oldVal instanceof Integer) {
+                        Float fval = Float.parseFloat(valStr);
+                        Integer val = Math.round(fval);
+                        currentParamMap.put(param, val);
+                    }
+                    return true;
+                } catch (NumberFormatException e) {
+                    System.out.println(format("Conversion error for parameter %s : %s", param, e.toString()));
+                }
+            }
+        }
+        return false;
     }
 
     public void setConsole(@NotNull TextArea console) {
@@ -103,9 +239,11 @@ public class SerialDevice {
             port.openPort();
             port.setParams(baudRate, dataBits, stopBits, jsscParity);
             portState = SerialState.IDLE;
+            readThreadShouldExit = false;
             new Thread(serialReader).start();
         } catch (SerialPortException e) {
             LOG.error("Cannot open port {}, retrying in {}ms", getPortSpec(), reopenTimeoutMS, e);
+            readThreadShouldExit = true;
             //reopenAt = System.currentTimeMillis() + reopenTimeoutMS;
             throw e;
         }
@@ -204,6 +342,7 @@ public class SerialDevice {
                 portState = SerialState.IDLE;
             } else {
                 portState = SerialState.NOT_OPEN;
+                readThreadShouldExit = true;
                 port.closePort();
                 throw new IOException(String.format("Failed to write to %s, closing", getPortSpec()));
             }
@@ -284,6 +423,7 @@ public class SerialDevice {
             //TODO throw exception
         } else {
             try {
+                readThreadShouldExit = true;
                 port.closePort();
                 portState = SerialState.NOT_OPEN;
             } catch (SerialPortException e) {
